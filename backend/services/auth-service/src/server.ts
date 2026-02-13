55be6617f78bb6ccc3bcc4376dcd58284c4587c4
Minitsonga <minitsonga2002@gmail.com>
2026-02-13 15:57:58 +0100

    Add type to Port const

--- backend/services/auth-service/src/server.ts
+++ backend/services/auth-service/src/server.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT;
 
 // Middleware
 app.use(express.json());
